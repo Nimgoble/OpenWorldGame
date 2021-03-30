a69dd8f27e5129fd908250a01a2efe932e0d0191
Nimgoble <nimgoble@gmail.com>
2021-03-29 20:49:30 -0600

    Check to make sure build object is in camp.

---
+++ Source/OpenWorldGame/OWGFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OWGFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWORLDGAME_API UOWGFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
